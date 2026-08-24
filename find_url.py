19a1d335e81d5c4da9f7384191feb02d52e88d32
Carlos Marin <raulmarincarlos@gmail.com>
2026-08-23 20:31:23 -0500

    Clean up unused scripts and fix Excel/PDF export bugs

--- find_url.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('spline_page.html', encoding='utf-16le') as f:
-    content = f.read()
-match = re.search(r'https://[^\"\'\s>]+\.splinecode', content)
-if match:
-    print(match.group(0))
-else:
-    print("NOT FOUND")
